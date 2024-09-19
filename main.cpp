a4281cc8a82f0bab691e388abdbebc6cbb6dba83
Jonathan Velasquez <johedvelca@gmail.com>
2024-09-18 23:17:59 -0400

    readme updated, prop render style modified

--- main.cpp
+++ main.cpp
@@ -21,7 +21,10 @@ int main()
 
     Character knight(windowWidth, windowHeight);
 
-    Prop rock{Vector2{0.f, 0.f}, LoadTexture("nature_tileset/Rock.png")};
+    Prop props[2]{
+        Prop{Vector2{600.f, 300.f}, LoadTexture("nature_tileset/Rock.png")},
+        Prop{Vector2{400.f, 500.f}, LoadTexture("nature_tileset/Log.png")}
+    };
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -35,7 +38,11 @@ int main()
         // Draw Map
         DrawTextureEx(map, mapPos, 0.0, mapScale, WHITE);
         
-        rock.Render(knight.getWorldPos());
+        for (auto prop : props)
+        {
+            prop.Render(knight.getWorldPos());
+        }
+
         knight.tick(GetFrameTime());
 
         //check map bounds
