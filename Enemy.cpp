3f58ad73c4d863b74ecd05956ffcf21fdacdcc79
Jonathan Velasquez <johedvelca@gmail.com>
2024-09-27 10:58:37 -0400

    Enemy AI

--- Enemy.cpp
+++ Enemy.cpp
@@ -10,10 +10,23 @@ Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture)
     run = run_texture;
     width = texture.width/maxFrames;
     height = texture.height;
+    speed = 3.5f;
 }
 
 void Enemy::tick(float deltaTime)
 {
+    // Get the toTarget vector
+    Vector2 toTarget = Vector2Subtract(target->getScreenPos(), screenPos);
+
+    // Normalize the toTarget vector
+    toTarget = Vector2Normalize(toTarget);
+
+    // Multiply the toTarget vector by speed
+    toTarget = Vector2Scale(toTarget, speed);
+
+    // Move the Enemy (set worldPos)
+    worldPos = Vector2Add(worldPos, toTarget);
+
     screenPos = Vector2Subtract(worldPos, target->getWorldPos());
     BaseCharacter::tick(deltaTime);
 }
